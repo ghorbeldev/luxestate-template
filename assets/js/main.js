cf23ee7421092e93233f049bb71963d19650d02c
ghorbeldev <adem.ghorbel9@gmail.com>
2022-02-19 17:30:27 +0100

    Vibo Template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Shrink Header On Scroll
-const header = document.querySelector('.header > .container');
+const header = document.querySelector('.header');
 window.onscroll = () => {
 	if (
 		document.body.scrollTop > 100 ||
@@ -10,16 +10,4 @@ window.onscroll = () => {
 		header.classList.remove('shrink');
 	}
 };
-// Toggle Navbar
-const navbarToggler = document.querySelector('.navbar__toggler');
-const navbar = document.querySelector('nav.navbar');
-navbarToggler.onclick = e => {
-	navbarToggler.classList.toggle('open');
-	if (navbarToggler.classList.contains('open')) {
-		navbar.classList.add('active');
-		document.body.classList.add('overlay');
-	} else {
-		navbar.classList.remove('active');
-		document.body.classList.remove('overlay');
-	}
-};
+// Toggle Active Menu
